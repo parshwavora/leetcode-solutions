e67f1c2bc5dc716123127099e33b75745b018a5c
Apple <apple@Apples-MacBook-Pro.local>
2025-07-03 00:58:09 +0530

    [problem]:longestcommonprefix

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,1 +1,18 @@
-xyz
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    let res = "";
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let s of strs) {
+            if (i === s.length || s[i] !== strs[0][i]) {
+                console.log(res)
+                return res;
+            }
+        }
+        res += strs[0][i];
+    }
+    console.log(res)
+    return res;
+};
+
+longestCommonPrefix(['flower','flow','fluid'])
